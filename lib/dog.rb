92db1fcfae31c33b66f2272656f0e5a33f3b9906
Christina Howard <clhoward33@icloud.com>
2020-09-19 19:07:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Add your code here
+class Dog()
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    puts self.map {|dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end   
